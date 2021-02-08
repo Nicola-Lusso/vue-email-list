ab3cd61c623ceb3fe663de9d2fda089818d90315
Nicola <nicolusso@gmail.com>
2021-02-08 16:19:55 +0100

    fine

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    email:[],
+
+  },
+  mounted() {
+
+    for (let i = 0; i < 10; i++) {
+
+      axios
+
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((result) => {
+        this.email.push(result.data.response);
+
+
+      })
+    }
+  }
+});
